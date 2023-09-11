35b59b3e57ae49dbb39fe84aa2c3c1e67af91761
Li Yazhou <me.ssword@gmail.com>
2023-09-11 22:14:20 +0800

    add type

--- src/gguf.rs
+++ src/gguf.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 const GGUF_MAGIC: u64 = 0x46554747;
 const GGUF_VERSION: u64 = 2;
 const GGUF_DEFAULT_ALIGNMENT: u64 = 32;
@@ -61,7 +63,7 @@ pub enum ModelArch {
 }
 
 #[derive(Debug)]
-pub enum MODEL_TENSOR {
+pub enum ModelTensor {
     TOKEN_EMBD = 0,
     POS_EMBD = 1,
     OUTPUT = 2,
@@ -79,4 +81,82 @@ pub enum MODEL_TENSOR {
     FFN_DOWN = 14,
     FFN_UP = 15,
     FFN_NORM = 16,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ValueType {
+    // The value is a 8-bit unsigned integer.
+    U8 = 0,
+    // The value is a 8-bit signed little-endian integer.
+    I8 = 1,
+    // The value is a 16-bit unsigned little-endian integer.
+    U16 = 2,
+    // The value is a 16-bit signed little-endian integer.
+    I16 = 3,
+    // The value is a 32-bit unsigned little-endian integer.
+    U32 = 4,
+    // The value is a 32-bit signed little-endian integer.
+    I32 = 5,
+    // The value is a 32-bit IEEE754 floating point number.
+    F32 = 6,
+    // The value is a boolean.
+    // 1-byte value where 0 is false and 1 is true.
+    // Anything else is invalid, and should be treated as either the model being invalid or the reader being buggy.
+    Bool = 7,
+    // The value is a UTF-8 non-null-terminated string, with length prepended.
+    String = 8,
+    // The value is an array of other values, with the length and type prepended.
+    ///
+    // Arrays can be nested, and the length of the array is the number of elements in the array, not the number of bytes.
+    Array = 9,
+    // The value is a 64-bit unsigned little-endian integer.
+    U64 = 10,
+    // The value is a 64-bit signed little-endian integer.
+    I64 = 11,
+    // The value is a 64-bit IEEE754 floating point number.
+    F64 = 12,
+}
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    U8(u8),
+    I8(i8),
+    U16(u16),
+    I16(i16),
+    U32(u32),
+    I32(i32),
+    U64(u64),
+    I64(i64),
+    F32(f32),
+    F64(f64),
+    Bool(bool),
+    String(String),
+    Array(Vec<Value>),
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub enum GGUFReaderErrorKind {
+    Unexpected,
+    DataError,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct GGUFError {
+    kind: GGUFReaderErrorKind,
+    msg: String,
+}
+
+pub type Result<T> = std::result::Result<T, GGUFError>;
+
+pub struct GGUFReader<R: Read> {
+    r: R,
+    arch: String,
+}
+
+impl<R> GGUFReader<R>
+where
+    R: Read,
+{
+    pub fn read_string(&mut self) -> Result<String> {
+    }
 }

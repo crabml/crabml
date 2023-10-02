5a4c91b1918264b695ecafb364bcdb4f12b3fe09
Li Yazhou <me.ssword@gmail.com>
2023-10-02 13:31:15 +0800

    refactor tensor

--- crabml-core/src/tensor/tensor.rs
+++ crabml-core/src/tensor/tensor.rs
@@ -5,12 +5,13 @@ use std::{cell::RefCell, rc::Rc};
 pub struct TensorStrider {
     shape: Vec<usize>,
     strides: Vec<usize>,
+    offset: usize,
 }
 
 impl TensorStrider {
-    pub fn new(shape: Vec<usize>) -> Self {
+    pub fn new(shape: Vec<usize>, offset: usize) -> Self {
         let strides = Self::compute_strides(&shape);
-        Self { shape, strides }
+        Self { shape, strides, offset }
     }
 
     pub fn shape(&self) -> &[usize] {
@@ -27,6 +28,28 @@ impl TensorStrider {
             offset += dim * stride;
         }
         offset
+    }
+
+    pub fn row(&self, pos: &[usize]) -> Result<TensorStrider> {
+        if pos.len() >= self.shape.len() {
+            return Err((
+                ErrorKind::TensorError,
+                format!(
+                    "invalid row position {:?} for tensor of shape {:?}",
+                    pos, self.shape
+                )).into()
+            );
+        }
+
+        let offset = pos
+            .iter()
+            .zip(self.strides.iter())
+            .map(|(&p, &s)| p * s)
+            .sum();
+
+        let shape = self.shape[pos.len()..].to_vec();
+        let strides = self.strides[pos.len()..].to_vec();
+        Ok(TensorStrider { shape, strides, offset })
     }
 
     pub fn len(&self) -> usize {
@@ -74,9 +97,9 @@ pub trait TensorDevice {
 
     fn recycle_buffer(&mut self, handle: TensorBufferHandle);
 
-    fn copy_buffer(&mut self, handle: TensorBufferHandle, offset: usize, buf: &[u8]);
+    fn copy_buffer(&mut self, dst: TensorBufferHandle, src: TensorBufferHandle, offset: usize, len: usize);
 
-    fn register_buffer(&mut self, buf: &[u8]);
+    fn register_buffer(&mut self, buf: &[u8]) -> TensorBufferHandle;
 
     fn retrieve_buffer(&self, handle: TensorBufferHandle) -> &[u8];
 }
@@ -84,14 +107,15 @@ pub trait TensorDevice {
 #[derive(Clone)]
 pub struct Tensor<D: TensorDevice> {
     strider: TensorStrider,
+    elem_size: usize,
     buf: Rc<RefCell<TensorBuffer<D>>>,
 }
 
 impl<D: TensorDevice> Tensor<D> {
     pub fn zeros(shape: Vec<usize>, device: &Rc<RefCell<D>>) -> Self {
-        let strider: TensorStrider = TensorStrider::new(shape);
+        let strider: TensorStrider = TensorStrider::new(shape, 0);
         let buf = TensorBuffer::alloc(strider.len(), device);
-        Self { strider, buf }
+        Self { strider, buf, elem_size: 8 }
     }
 
     pub fn shape(&self) -> &[usize] {
@@ -102,7 +126,32 @@ impl<D: TensorDevice> Tensor<D> {
         self.strider.len()
     }
 
-    pub fn view(&self, shape: Vec<usize>) -> Result<Self> {
+    pub fn row(&self, pos: &[usize]) -> Result<Self> {
+        let strider = self.strider.row(pos)?;
+        Ok(Self {
+            strider,
+            elem_size: self.elem_size,
+            buf: self.buf.clone(),
+        })
+    }
+
+    pub fn copy_row_from(&mut self, pos: &[usize], t: &Self) -> Result<()> {
+        let strider = self.strider.row(pos)?;
+        let n_elems = strider.len();
+        let src_len = n_elems * self.elem_size;
+        let dst_offset = strider.offset * self.elem_size;
+        let dst = self.buf.borrow_mut();
+        let src = t.buf.borrow();
+        dst.device.borrow_mut().copy_buffer(
+            dst.handle,
+            dst_offset,
+            src.handle,
+            src_len,
+        );
+        Ok(())
+    }
+
+    pub fn reshape(&self, shape: Vec<usize>) -> Result<Self> {
         let len: usize = shape.iter().product();
         if len != self.len() {
             return Err((
@@ -114,9 +163,10 @@ impl<D: TensorDevice> Tensor<D> {
             )
                 .into());
         }
-        let strider = TensorStrider::new(shape);
+        let strider = TensorStrider::new(shape, self.strider.offset);
         Ok(Self {
             strider,
+            elem_size: self.elem_size,
             buf: self.buf.clone(),
         })
     }

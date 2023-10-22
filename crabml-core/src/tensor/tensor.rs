c736c8f10b1f968a1c4d2f1b1abea02f14777ecc
Li Yazhou <me.ssword@gmail.com>
2023-10-22 10:33:33 +0800

    refactor with TensorOpVar

--- crabml-core/src/tensor/tensor.rs
+++ crabml-core/src/tensor/tensor.rs
@@ -7,58 +7,58 @@ use crate::tensor::strider::TensorStrider;
 
 type TensorID = usize;
 
-pub enum TensorComputeOp {
+type TensorBufID = usize;
+
+#[derive(Clone, Debug)]
+pub struct TensorOpVar {
+    buf_id: usize,
+    strider: TensorStrider,
+}
+
+#[derive(Clone, Debug)]
+pub enum TensorOp {
     AllocTensor {
         strider: TensorStrider,
     },
 
     RecycleTensor {
-        t: TensorID,
-    },
-
-    EditTensor {
-        t: TensorID,
-        strider: TensorStrider,
-    },
-
-    RefTensor {
-        t: TensorID,
+        t: TensorOpVar,
     },
 
     CopyFrom {
-        dst: TensorID,
+        dst: TensorOpVar,
         pos: Vec<usize>,
-        src: TensorID,
+        src: TensorOpVar,
     },
 
     MatMul {
-        out: TensorID,
-        lhs: TensorID,
-        rhs: TensorID,
+        out: TensorOpVar,
+        lhs: TensorOpVar,
+        rhs: TensorOpVar,
     },
 
     RopeInplace {
-        t: TensorID,
+        t: TensorOpVar,
         pos: usize,
         rope_dim: usize,
     },
 
     SiluInplace {
-        t: TensorID,
+        t: TensorOpVar,
     },
 
     MulInplace {
-        t1: TensorID,
-        t2: TensorID,
+        t1: TensorOpVar,
+        t2: TensorOpVar,
     },
 
     AddInplace {
-        t1: TensorID,
-        t2: TensorID,
+        t1: TensorOpVar,
+        t2: TensorOpVar,
     },
 
     RmsNormInplace {
-        t: TensorID,
+        t: TensorOpVar,
     },
 }
 
@@ -73,11 +73,9 @@ pub enum TensorComputeOp {
 /// may located in the CPU or GPU memory, you can not directly acccess its data except
 /// calling `export_tensor()` to load the tensor's data into the host's memory.
 pub trait TensorBackend {
-    fn append_op(&mut self, op: TensorComputeOp) -> Result<Option<TensorID>>;
+    fn append_op(&mut self, op: TensorOp) -> Result<Option<TensorOpVar>>;
 
-    fn process_op(&mut self, op: TensorComputeOp) -> Result<Option<TensorID>>;
-
-    fn import_tensor(&mut self, shape: &[usize], buf: &CpuTensorBuf) -> TensorID;
+    fn import_tensor(&mut self, shape: &[usize], buf: &CpuTensorBuf) -> TensorOpVar;
 
     fn export_tensor(self, t: TensorID, data: &mut [f32]) -> Result<()>;
 
@@ -86,7 +84,7 @@ pub trait TensorBackend {
 
 #[derive(Clone)]
 pub struct Tensor<D: TensorBackend> {
-    id: TensorID,
+    buf_id: TensorBufID,
     strider: TensorStrider,
     backend: Rc<RefCell<D>>,
 }
@@ -94,22 +92,30 @@ pub struct Tensor<D: TensorBackend> {
 impl<D: TensorBackend> Tensor<D> {
     pub fn zeros(shape: Vec<usize>, backend: Rc<RefCell<D>>) -> Result<Self> {
         let strider: TensorStrider = TensorStrider::new(shape.clone());
-        let id = backend
+        let op_var = backend
             .borrow_mut()
-            .append_op(TensorComputeOp::AllocTensor {
+            .append_op(TensorOp::AllocTensor {
                 strider: strider.clone(),
             })?
             .unwrap();
         Ok(Self {
-            id,
+            buf_id: op_var.buf_id,
             strider,
             backend,
         })
     }
 
+    fn as_op_var(&self) -> TensorOpVar {
+        TensorOpVar {
+            buf_id: self.buf_id,
+            strider: self.strider.clone(),
+        }
+    }
+
     pub fn shape(&self) -> &[usize] {
         self.strider.shape()
     }
+
 
     pub fn len(&self) -> usize {
         self.strider.len()
@@ -118,32 +124,30 @@ impl<D: TensorBackend> Tensor<D> {
     pub fn copy_from(&mut self, pos: &[usize], src: &Self) -> Result<()> {
         self.backend
             .borrow_mut()
-            .append_op(TensorComputeOp::CopyFrom {
-                dst: self.id,
+            .append_op(TensorOp::CopyFrom {
+                dst: self.as_op_var(),
                 pos: pos.to_vec(),
-                src: src.id,
+                src: src.as_op_var(),
             })?;
         Ok(())
     }
 
     pub fn view(self, shape: Vec<usize>) -> Result<Self> {
         let strider = self.strider.view(shape)?;
-
-        self.backend
-            .borrow_mut()
-            .append_op(TensorComputeOp::EditTensor {
-                t: self.id,
-                strider: strider.clone(),
-            })?;
         Ok(Self {
             strider,
-            id: self.id,
+            buf_id: self.buf_id,
             backend: self.backend.clone(),
         })
     }
 
-    pub fn repeat(self, _n: usize, _axis: usize) -> Result<Self> {
-        todo!()
+    pub fn repeat(self, repeats: Vec<usize>) -> Result<Self> {
+        let strider = self.strider.repeat(repeats)?;
+        Ok(Self {
+            strider,
+            buf_id: self.buf_id,
+            backend: self.backend.clone(),
+        })
     }
 
     pub fn mul(self, _t: &Self) -> Result<Self> {
@@ -163,7 +167,7 @@ impl<D: TensorBackend> Drop for Tensor<D> {
     fn drop(&mut self) {
         self.backend
             .borrow_mut()
-            .append_op(TensorComputeOp::RecycleTensor { t: self.id })
+            .append_op(TensorOp::RecycleTensor { t: self.as_op_var() })
             .unwrap();
     }
 }

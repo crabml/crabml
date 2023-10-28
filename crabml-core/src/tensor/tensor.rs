b0f9a7cc106e3a4dcf7cfca8026e5012b053fb96
Li Yazhou <me.ssword@gmail.com>
2023-10-28 14:05:20 +0800

    use trait to abstract tensors

--- crabml-core/src/tensor/tensor.rs
+++ crabml-core/src/tensor/tensor.rs
@@ -1,166 +1,42 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
 use crate::error::Result;
-use crate::tensor::strider::TensorStrider;
+use crate::gguf::GGMLType;
 
-type TensorID = usize;
+pub trait TensorBackend<T> {}
 
-pub enum TensorDeviceOp {
-    AllocTensor {
-        strider: TensorStrider,
-    },
+pub trait Tensor: Sized + Clone + TensorArithmetics {
+    type Backend;
 
-    RecycleTensor {
-        t: TensorID,
-    },
+    fn alloc(shape: &[usize], backend: Self::Backend) -> Result<Self>;
 
-    EditTensor {
-        t: TensorID,
-        strider: TensorStrider,
-    },
+    fn from_bytes(bytes: &[u8], typ: GGMLType, shape: &[usize], backend: Self::Backend) -> Result<()>;
 
-    CopyFrom {
-        dst: TensorID,
-        pos: Vec<usize>,
-        src: TensorID,
-    },
+    fn view(self, shape: &[usize]) -> Result<Self>;
 
-    MatMul {
-        out: TensorID,
-        lhs: TensorID,
-        rhs: TensorID,
-    },
+    fn transpose(self, shape: &[usize]) -> Result<Self>;
 
-    RopeInplace {
-        q: TensorID,
-        k: TensorID,
-        pos: usize,
-        freq_base: f32,
-        freq_scale: f32,
-    },
+    fn as_ref(&self) -> Self;
 
-    SiluInplace {
-        t: TensorID,
-    },
+    fn extend(&mut self, rhs: &Self) -> Result<()>;
 
-    MulInplace {
-        t1: TensorID,
-        t2: TensorID,
-    },
+    fn copy_from(&mut self, rhs: &Self, pos: &[usize], len: usize) -> Result<()>;
 
-    AddInplace {
-        t1: TensorID,
-        t2: TensorID,
-    },
-
-    RmsNormInplace {
-        t: TensorID,
-    },
+    fn export(&self, buf: &mut [f32]) -> Result<()>;
 }
 
-/// A tensor device is responsible for manage the buffer of tensors, and performing
-/// platform specific operations on tensors.
-///
-/// A tensor device might be a CPU, GPU. Besides that, if we want to inference a model
-/// with quantization, we can also have a specialized quantized CPU/GPU tensor device.
-///
-/// The tensors are managed in a handle based way. The device is responsible for manage
-/// the pool of the tensors, each tensor is identified by a unique TensorID. The tensor
-/// may located in the CPU or GPU memory, you can not directly acccess its data except
-/// calling `export_tensor()` to load the tensor's data into the host's memory.
-pub trait TensorDevice {
-    type DataType;
+pub trait TensorArithmetics: Sized {
+    fn mul_inplace(x: &mut Self, y: &Self) -> Result<()>;
 
-    fn process_op(&mut self, op: TensorDeviceOp) -> Result<Option<TensorID>>;
+    fn add_inplace(x: &mut Self, y: &Self) -> Result<()>;
 
-    fn import_tensor(&mut self, shape: &[usize], data: &[Self::DataType]) -> TensorID;
+    fn div_scalar_inplace(x: &mut Self, y: f32) -> Result<()>;
 
-    fn export_tensor(self, t: TensorID, data: &mut [Self::DataType]) -> Result<()>;
+    fn matmul(x: &mut Self, y: &Self) -> Result<Self>;
 
-    fn name(&self) -> &'static str;
-}
+    fn batch_matmul(x: &mut Self, y: &Self) -> Result<Self>;
 
-#[derive(Clone)]
-pub struct Tensor<D: TensorDevice> {
-    id: TensorID,
-    strider: TensorStrider,
-    device: Rc<RefCell<D>>,
-}
+    fn silu_inplace(x: &mut Self) -> Result<()>;
 
-impl<D: TensorDevice> Tensor<D> {
-    pub fn zeros(shape: Vec<usize>, device: Rc<RefCell<D>>) -> Result<Self> {
-        let strider: TensorStrider = TensorStrider::new(shape.clone());
-        let id = device
-            .borrow_mut()
-            .process_op(TensorDeviceOp::AllocTensor {
-                strider: strider.clone(),
-            })?
-            .unwrap();
-        Ok(Self {
-            id,
-            strider,
-            device,
-        })
-    }
+    fn rms_norm_inplace(x: &mut Self, eps: f32) -> Result<()>;
 
-    pub fn shape(&self) -> &[usize] {
-        self.strider.shape()
-    }
-
-    pub fn len(&self) -> usize {
-        self.strider.len()
-    }
-
-    pub fn copy_from(&mut self, pos: &[usize], src: &Self) -> Result<()> {
-        self.device
-            .borrow_mut()
-            .process_op(TensorDeviceOp::CopyFrom {
-                dst: self.id,
-                pos: pos.to_vec(),
-                src: src.id,
-            })?;
-        Ok(())
-    }
-
-    pub fn view(self, shape: Vec<usize>) -> Result<Self> {
-        let strider = self.strider.view(shape)?;
-
-        self.device
-            .borrow_mut()
-            .process_op(TensorDeviceOp::EditTensor {
-                t: self.id,
-                strider: strider.clone(),
-            })?;
-        Ok(Self {
-            strider,
-            id: self.id,
-            device: self.device.clone(),
-        })
-    }
-
-    pub fn repeat(self, _n: usize, _axis: usize) -> Result<Self> {
-        todo!()
-    }
-
-    pub fn mul(self, _t: &Self) -> Result<Self> {
-        todo!()
-    }
-
-    pub fn add(self, _t: &Self) -> Result<Self> {
-        todo!()
-    }
-
-    pub fn matmul(&self, _t: &Self) -> Result<Self> {
-        todo!()
-    }
-}
-
-impl<D: TensorDevice> Drop for Tensor<D> {
-    fn drop(&mut self) {
-        self.device
-            .borrow_mut()
-            .process_op(TensorDeviceOp::RecycleTensor { t: self.id })
-            .unwrap();
-    }
+    fn rope_inplace(x: &mut Self, pos: usize, rope_dims: usize) -> Result<()>;
 }

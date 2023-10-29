13c73b586750bdc887caa7e6afd7988cd35587c5
flaneur <me.ssword@gmail.com>
2023-10-29 11:48:44 +0800

    Revert "refactor: use a simply trait to replace the op code machine (#11)" (#12)
    This reverts commit 4e7c360a7af2b21b00db4cb77c9241534d9d89f6.

--- crabml-core/src/tensor/cpu/arithmetic.rs
+++ crabml-core/src/tensor/cpu/arithmetic.rs
@@ -16,194 +16,27 @@ use crate::tensor::cpu::validate::require_tensor_contiguous;
 use crate::tensor::cpu::validate::require_tensor_dims;
 use crate::tensor::cpu::validate::require_tensor_matmul_2d_shapes;
 use crate::tensor::cpu::validate::require_tensor_shape;
-use crate::tensor::tensor::Tensor;
-use crate::tensor::tensor::TensorArithmetics;
 use crate::tensor::CpuTensor;
 
 /// ! arithmetic.rs contains the tensor arithmetics operations like matmul, accum, etc.
 
-impl<'a> TensorArithmetics for CpuTensor<'a> {
-    fn mul_inplace(mut self, rhs: &Self) -> Result<Self> {
-        require_tensor_shape(&self, rhs.shape())?;
+pub fn rms_norm_inplace(mut x: CpuTensor<'_>, eps: f32) -> Result<CpuTensor<'_>> {
+    require_tensor_contiguous(&x)?;
+    require_tensor_dims(&x, &[1])?;
 
-        if rhs.is_contiguous() && rhs.is_contiguous() {
-            match (self.buf_mut(), rhs.buf()) {
-                (CpuTensorBuf::F32(Cow::Owned(ab)), CpuTensorBuf::F32(bb)) => {
-                    mul_inplace_vec_f32(ab, bb);
-                    return Ok(self);
-                }
-                _ => (),
-            }
+    match x.buf_mut() {
+        CpuTensorBuf::F32(Cow::Owned(xb)) => {
+            rms_norm_inplace_vec_f32(xb, eps);
+            return Ok(x);
         }
-
-        for (ia, ib) in self.iter_mut()?.zip(rhs.iter()) {
-            *ia *= ib;
-        }
-        Ok(self)
+        _ => (),
     }
 
-    fn add_inplace(self, b: &Self) -> Result<Self> {
-        let mut a = self;
-        require_tensor_shape(&a, b.shape())?;
-        require_tensor_contiguous(&a)?;
-        require_tensor_contiguous(b)?;
-
-        a.iter_mut()?.zip(b.iter()).for_each(|(ia, ib)| {
-            *ia += ib;
-        });
-        Ok(a)
-    }
-
-    fn div_scalar_inplace(mut self, b: f32) -> Result<Self> {
-        self.iter_mut()?.for_each(|ia| {
-            *ia /= b;
-        });
-        Ok(self)
-    }
-
-    // W (w_rows,w_cols) @ x (w_cols,x_cols) -> xout (w_rows,x_cols)
-    // W (w_rows,w_cols) @ x (w_cols,) -> xout (w_rows,)
-    fn matmul(&self, x: &Self) -> Result<Self> {
-        let w = self;
-        require_tensor_dims(w, &[2])?;
-        require_tensor_dims(x, &[1])?;
-        require_tensor_matmul_2d_shapes(w, x)?;
-        require_tensor_contiguous(w)?;
-        require_tensor_contiguous(x)?;
-
-        match maybe_matmul_vec_2d_1d(w, x) {
-            Some(r) => return r,
-            _ => (),
-        }
-
-        let mut out = CpuTensor::alloc(&[w.shape()[0]], w.pool())?;
-        let o_row_iter = out.iter_axis_mut(vec![0], 0)?; // (x_cols, )
-        o_row_iter.enumerate().for_each(|(w_row, o)| {
-            let w_row_iter = w.iter_axis(&[w_row, 0], 1).unwrap(); // (w_cols, )
-            let x_col_iter = x.iter_axis(&[0], 0).unwrap(); // (w_cols, )
-            *o = w_row_iter.zip(x_col_iter).map(|(w, x)| w * x).sum::<f32>();
-        });
-        return Ok(out);
-    }
-
-    fn batch_matmul(&self, x: &Self) -> Result<Self> {
-        todo!()
-    }
-
-    fn silu_inplace(self) -> Result<Self> {
-        let mut x = self;
-        if x.is_contiguous() {
-            if let CpuTensorBuf::F32(Cow::Owned(xb)) = x.buf_mut() {
-                silu_inplace_vec_f32(xb);
-                return Ok(x);
-            }
-        }
-        x.iter_mut()?.for_each(|n| *n = *n / (1.0 + (-*n).exp()));
-        Ok(x)
-    }
-
-    fn rms_norm_inplace(mut self, eps: f32) -> Result<Self> {
-        require_tensor_contiguous(&self)?;
-        require_tensor_dims(&self, &[1])?;
-
-        match self.buf_mut() {
-            CpuTensorBuf::F32(Cow::Owned(xb)) => {
-                rms_norm_inplace_vec_f32(xb, eps);
-                return Ok(self);
-            }
-            _ => (),
-        }
-
-        let len = self.shape()[0];
-        let sum = self.iter_axis(&[0], 0)?.fold(0.0, |s, n| s + n * n);
-        let rms = ((sum / len as f32) + eps).sqrt();
-        self.iter_axis_mut(vec![0], 0)?.for_each(|n| *n = *n / rms);
-        Ok(self)
-    }
-
-    fn rope_inplace(self, pos: usize, rope_dims: usize) -> Result<Self> {
-        let mut q = self;
-        require_tensor_contiguous(&q)?;
-        require_tensor_dims(&q, &[2])?;
-
-        let n_heads = q.shape()[0];
-        let head_size = q.shape()[1];
-        let qb = q.f32_buf_mut()?;
-
-        // apply RoPE rotation for each head
-        for h in 0..n_heads {
-            for i in 0..rope_dims / 2 {
-                let theta_scale = 10000_f32.powf(-2.0 * i as f32 / head_size as f32);
-                let theta = pos as f32 * theta_scale;
-
-                let cos_theta = theta.cos();
-                let sin_theta = theta.sin();
-                let qp = &mut qb[h * head_size + i * 2..];
-                let qp0 = qp[0];
-                let qp1 = qp[1];
-                qp[0] = qp0 * cos_theta - qp1 * sin_theta;
-                qp[1] = qp0 * sin_theta + qp1 * cos_theta;
-            }
-        }
-
-        Ok(q)
-    }
-
-    fn softmax_inplace(self, axis: usize) -> Result<Self> {
-        let mut t = self;
-        require_tensor_dims(&t, &[2])?;
-
-        if axis != 1 {
-            return Err((ErrorKind::TensorError, "only axis=1 is supported").into());
-        }
-
-        for row in 0..t.shape()[0] {
-            let max = t.iter_axis(&[row, 0], 1)?.fold(f32::NAN, |a, b| a.max(b));
-            let sum = t.iter_axis_mut(vec![row, 0], 1)?.fold(0.0, |mut acc, val| {
-                *val = (*val - max).exp();
-                acc += *val;
-                acc
-            });
-            t.iter_axis_mut(vec![row, 0], 1)?.for_each(|val| {
-                *val /= sum;
-            });
-        }
-
-        Ok(t)
-    }
-}
-
-
-pub fn batch_matmul<'a, 'b>(w: &CpuTensor<'a>, x: &CpuTensor<'b>) -> Result<CpuTensor<'b>>
-where 'b: 'a {
-    require_tensor_dims(w, &[3])?;
-    require_tensor_dims(x, &[2])?;
-
-    if w.shape()[0] != x.shape()[0] || w.shape()[2] != x.shape()[1] {
-        return Err((
-            ErrorKind::TensorError,
-            format!(
-                "mismatched tensor shapes on batch matmul: {:?} @ {:?}",
-                w.shape(),
-                x.shape()
-            ),
-        )
-            .into());
-    }
-
-    // (batch_size, w_rows, w_cols) @ (batch_size, w_cols, ) -> (batch_size, w_rows, )
-    let batch_size = w.shape()[0];
-    let w_rows = w.shape()[1];
-    let mut out = CpuTensor::alloc(&[batch_size, w_rows], x.pool())?;
-    for b in 0..batch_size {
-        let o_iter = out.iter_axis_mut(vec![b, 0], 1)?; // w_cols
-        o_iter.enumerate().for_each(|(w_row, o)| {
-            let w_iter = w.iter_axis(&[b, w_row, 0], 2).unwrap(); // w_rows
-            let x_iter = x.iter_axis(&[b, 0], 1).unwrap(); // w_rows
-            *o = w_iter.zip(x_iter).map(|(w, x)| w * x).sum::<f32>();
-        })
-    }
-    return Ok(out);
+    let len = x.shape()[0];
+    let sum = x.iter_axis(&[0], 0)?.fold(0.0, |s, n| s + n * n);
+    let rms = ((sum / len as f32) + eps).sqrt();
+    x.iter_axis_mut(vec![0], 0)?.for_each(|n| *n = *n / rms);
+    Ok(x)
 }
 
 fn rms_norm_inplace_vec_f32(x: &mut [f32], eps: f32) {
@@ -223,6 +56,25 @@ fn rms_norm_inplace_vec_f32(x: &mut [f32], eps: f32) {
     }
 }
 
+pub fn mul_inplace<'a>(mut a: CpuTensor<'a>, b: &CpuTensor<'a>) -> Result<CpuTensor<'a>> {
+    require_tensor_shape(&a, b.shape())?;
+
+    if a.is_contiguous() && b.is_contiguous() {
+        match (a.buf_mut(), b.buf()) {
+            (CpuTensorBuf::F32(Cow::Owned(ab)), CpuTensorBuf::F32(bb)) => {
+                mul_inplace_vec_f32(ab, bb);
+                return Ok(a);
+            }
+            _ => (),
+        }
+    }
+
+    for (ia, ib) in a.iter_mut()?.zip(b.iter()) {
+        *ia *= ib;
+    }
+    Ok(a)
+}
+
 fn mul_inplace_vec_f32(a: &mut [f32], b: &[f32]) {
     let ac = a.as_chunks_mut::<32>().0;
     let bc = b.as_chunks::<32>().0;
@@ -234,6 +86,24 @@ fn mul_inplace_vec_f32(a: &mut [f32], b: &[f32]) {
     });
 }
 
+pub fn div_scalar_inplace<'a>(mut a: CpuTensor<'a>, b: f32) -> Result<CpuTensor<'a>> {
+    a.iter_mut()?.for_each(|ia| {
+        *ia /= b;
+    });
+    Ok(a)
+}
+
+pub fn add_inplace<'a>(mut a: CpuTensor<'a>, b: &CpuTensor<'a>) -> Result<CpuTensor<'a>> {
+    require_tensor_shape(&a, b.shape())?;
+    require_tensor_contiguous(&a)?;
+    require_tensor_contiguous(b)?;
+
+    a.iter_mut()?.zip(b.iter()).for_each(|(ia, ib)| {
+        *ia += ib;
+    });
+    Ok(a)
+}
+
 pub fn add_inplace_vec_f32(a: &mut [f32], b: &[f32]) {
     let acs = a.as_chunks_mut::<32>().0;
     let bcs = b.as_chunks::<32>().0;
@@ -243,6 +113,20 @@ pub fn add_inplace_vec_f32(a: &mut [f32], b: &[f32]) {
         va += vb;
         va.copy_to_slice(ac);
     });
+}
+
+pub fn silu_inplace<'a>(mut x: CpuTensor<'a>) -> Result<CpuTensor<'a>> {
+    // for i in 0..buf.len() {
+    //    buf[i] = buf[i] * (1.0 / (1.0 + (-buf[i]).exp()));
+    // }
+    if x.is_contiguous() {
+        if let CpuTensorBuf::F32(Cow::Owned(xb)) = x.buf_mut() {
+            silu_inplace_vec_f32(xb);
+            return Ok(x);
+        }
+    }
+    x.iter_mut()?.for_each(|n| *n = *n / (1.0 + (-*n).exp()));
+    Ok(x)
 }
 
 pub fn silu_inplace_vec_f32(buf: &mut [f32]) {
@@ -263,6 +147,30 @@ pub fn silu_inplace_vec_f32(buf: &mut [f32]) {
         let v3 = v0 / v2;
         v3.copy_to_slice(chunk);
     })
+}
+
+// W (w_rows,w_cols) @ x (w_cols,x_cols) -> xout (w_rows,x_cols)
+// W (w_rows,w_cols) @ x (w_cols,) -> xout (w_rows,)
+pub fn matmul_2d_1d<'a>(w: &CpuTensor<'a>, x: &CpuTensor<'a>) -> Result<CpuTensor<'a>> {
+    require_tensor_dims(w, &[2])?;
+    require_tensor_dims(x, &[1])?;
+    require_tensor_matmul_2d_shapes(w, x)?;
+    require_tensor_contiguous(w)?;
+    require_tensor_contiguous(x)?;
+
+    match maybe_matmul_vec_2d_1d(w, x) {
+        Some(r) => return r,
+        _ => (),
+    }
+
+    let mut out = CpuTensor::zeros(vec![w.shape()[0]])?;
+    let o_row_iter = out.iter_axis_mut(vec![0], 0)?; // (x_cols, )
+    o_row_iter.enumerate().for_each(|(w_row, o)| {
+        let w_row_iter = w.iter_axis(&[w_row, 0], 1).unwrap(); // (w_cols, )
+        let x_col_iter = x.iter_axis(&[0], 0).unwrap(); // (w_cols, )
+        *o = w_row_iter.zip(x_col_iter).map(|(w, x)| w * x).sum::<f32>();
+    });
+    return Ok(out);
 }
 
 pub fn maybe_matmul_vec_2d_1d<'a>(
@@ -287,7 +195,7 @@ pub fn maybe_matmul_vec_2d_1d<'a>(
         _ => return None,
     };
 
-    Some(CpuTensor::new(out, &[w.shape()[0]], w.pool()))
+    Some(CpuTensor::new(out, vec![w.shape()[0]]))
 }
 
 pub fn matmul_vec_generic_xxx_f32_2d_1d<'a, T: BufVecDot + Sync>(
@@ -334,6 +242,93 @@ pub fn matmul_vec_q8_0_f32_2d_1d<'a>(wb: &QuantBufQ8_0<'a>, xb: &[f32], out: &mu
         });
 }
 
+pub fn batch_matmul<'a, 'b>(w: &CpuTensor<'a>, x: &CpuTensor<'a>) -> Result<CpuTensor<'b>>
+where 'b: 'a {
+    require_tensor_dims(w, &[3])?;
+    require_tensor_dims(x, &[2])?;
+
+    if w.shape()[0] != x.shape()[0] || w.shape()[2] != x.shape()[1] {
+        return Err((
+            ErrorKind::TensorError,
+            format!(
+                "mismatched tensor shapes on batch matmul: {:?} @ {:?}",
+                w.shape(),
+                x.shape()
+            ),
+        )
+            .into());
+    }
+
+    // (batch_size, w_rows, w_cols) @ (batch_size, w_cols, ) -> (batch_size, w_rows, )
+    let batch_size = w.shape()[0];
+    let w_rows = w.shape()[1];
+    let mut out = CpuTensor::zeros(vec![batch_size, w_rows])?;
+    for b in 0..batch_size {
+        let o_iter = out.iter_axis_mut(vec![b, 0], 1)?; // w_cols
+        o_iter.enumerate().for_each(|(w_row, o)| {
+            let w_iter = w.iter_axis(&[b, w_row, 0], 2).unwrap(); // w_rows
+            let x_iter = x.iter_axis(&[b, 0], 1).unwrap(); // w_rows
+            *o = w_iter.zip(x_iter).map(|(w, x)| w * x).sum::<f32>();
+        })
+    }
+    return Ok(out);
+}
+
+// t: (rows, cols)
+pub fn softmax_inplace<'a>(mut t: CpuTensor<'a>, axis: usize) -> Result<CpuTensor<'a>> {
+    require_tensor_dims(&t, &[2])?;
+
+    if axis != 1 {
+        return Err((ErrorKind::TensorError, "only axis=1 is supported").into());
+    }
+
+    for row in 0..t.shape()[0] {
+        let max = t.iter_axis(&[row, 0], 1)?.fold(f32::NAN, |a, b| a.max(b));
+        let sum = t.iter_axis_mut(vec![row, 0], 1)?.fold(0.0, |mut acc, val| {
+            *val = (*val - max).exp();
+            acc += *val;
+            acc
+        });
+        t.iter_axis_mut(vec![row, 0], 1)?.for_each(|val| {
+            *val /= sum;
+        });
+    }
+
+    Ok(t)
+}
+
+pub fn rope_inplace<'a>(
+    mut q: CpuTensor<'a>,
+    pos: usize,
+    rope_dims: usize,
+) -> Result<CpuTensor<'a>> {
+    require_tensor_contiguous(&q)?;
+    require_tensor_dims(&q, &[2])?;
+
+    let n_heads = q.shape()[0];
+    let head_size = q.shape()[1];
+    let qb = q.f32_buf_mut()?;
+
+    // apply RoPE rotation for each head
+    for h in 0..n_heads {
+        for i in 0..rope_dims / 2 {
+            let theta_scale = 10000_f32.powf(-2.0 * i as f32 / head_size as f32);
+            let theta = pos as f32 * theta_scale;
+
+            let cos_theta = theta.cos();
+            let sin_theta = theta.sin();
+            let qp = &mut qb[h * head_size + i * 2..];
+            let qp0 = qp[0];
+            let qp1 = qp[1];
+            qp[0] = qp0 * cos_theta - qp1 * sin_theta;
+            qp[1] = qp0 * sin_theta + qp1 * cos_theta;
+        }
+    }
+
+    Ok(q)
+}
+
+// q: (n_heads, head_size)
 pub fn rope_inplace_old<'a>(
     mut q: CpuTensor<'a>,
     mut k: CpuTensor<'a>,
@@ -374,8 +369,6 @@ pub fn rope_inplace_old<'a>(
 
 #[cfg(test)]
 mod tests {
-    use crate::tensor::cpu::tensor::CpuTensorPool;
-
     use super::*;
 
     #[test]
@@ -407,17 +400,16 @@ mod tests {
     fn test_matmul() -> Result<()> {
         // 1, 2, 3
         // 4, 5, 6
-        let pool = CpuTensorPool::new();
-        let w = CpuTensor::new(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0], &[2, 3], pool.clone())?;
+        let w = CpuTensor::new(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0], vec![2, 3])?;
         // 1
         // 2
         // 3
-        let b = CpuTensor::new(vec![1.0, 2.0, 3.0], &[3], pool.clone())?;
+        let b = CpuTensor::new(vec![1.0, 2.0, 3.0], vec![3])?;
         // 0
         // 0
         // 1*1 + 2*2 + 3*3 = 1 + 4 + 9
         // 1*4 + 2*5 + 3*6 = 4 + 10 + 18
-        let out = w.matmul(&b)?;
+        let out = matmul_2d_1d(&w, &b)?;
         assert_eq!(out.iter().collect::<Vec<_>>(), &[14.0, 32.0]);
 
         Ok(())
--- crabml-core/src/tensor/tensor.rs
+++ crabml-core/src/tensor/tensor.rs
@@ -1,42 +1,166 @@
-use crate::{error::Result, gguf::GGMLType};
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub trait Tensor: Sized + Clone + TensorArithmetics {
-    type Pool;
+use crate::error::Result;
+use crate::tensor::strider::TensorStrider;
 
-    fn new(data: Vec<f32>, shape: &[usize], pool: Self::Pool) -> Result<Self>;
+type TensorID = usize;
 
-    fn alloc(shape: &[usize], pool: Self::Pool) -> Result<Self>;
+pub enum TensorDeviceOp {
+    AllocTensor {
+        strider: TensorStrider,
+    },
 
+    RecycleTensor {
+        t: TensorID,
+    },
 
-    fn view(self, shape: &[usize]) -> Result<Self>;
+    EditTensor {
+        t: TensorID,
+        strider: TensorStrider,
+    },
 
-    fn repeat(self, repeats: &[usize]) -> Result<Self>;
+    CopyFrom {
+        dst: TensorID,
+        pos: Vec<usize>,
+        src: TensorID,
+    },
 
-    fn transpose(self, shape: &[usize]) -> Result<Self>;
+    MatMul {
+        out: TensorID,
+        lhs: TensorID,
+        rhs: TensorID,
+    },
 
-    // fn as_ref(&'a self) -> Self;
+    RopeInplace {
+        q: TensorID,
+        k: TensorID,
+        pos: usize,
+        freq_base: f32,
+        freq_scale: f32,
+    },
 
-    fn extend(&mut self, rhs: &Self) -> Result<()>;
+    SiluInplace {
+        t: TensorID,
+    },
 
-    fn copy_from(&mut self, rhs: &Self, pos: &[usize], len: usize) -> Result<()>;
+    MulInplace {
+        t1: TensorID,
+        t2: TensorID,
+    },
+
+    AddInplace {
+        t1: TensorID,
+        t2: TensorID,
+    },
+
+    RmsNormInplace {
+        t: TensorID,
+    },
 }
 
-pub trait TensorArithmetics: Sized {
-    fn mul_inplace(self, y: &Self) -> Result<Self>;
+/// A tensor device is responsible for manage the buffer of tensors, and performing
+/// platform specific operations on tensors.
+///
+/// A tensor device might be a CPU, GPU. Besides that, if we want to inference a model
+/// with quantization, we can also have a specialized quantized CPU/GPU tensor device.
+///
+/// The tensors are managed in a handle based way. The device is responsible for manage
+/// the pool of the tensors, each tensor is identified by a unique TensorID. The tensor
+/// may located in the CPU or GPU memory, you can not directly acccess its data except
+/// calling `export_tensor()` to load the tensor's data into the host's memory.
+pub trait TensorDevice {
+    type DataType;
 
-    fn add_inplace(self, y: &Self) -> Result<Self>;
+    fn process_op(&mut self, op: TensorDeviceOp) -> Result<Option<TensorID>>;
 
-    fn div_scalar_inplace(self, y: f32) -> Result<Self>;
+    fn import_tensor(&mut self, shape: &[usize], data: &[Self::DataType]) -> TensorID;
 
-    fn matmul(&self, y: &Self) -> Result<Self>;
+    fn export_tensor(self, t: TensorID, data: &mut [Self::DataType]) -> Result<()>;
 
-    fn batch_matmul(&self, y: &Self) -> Result<Self>;
+    fn name(&self) -> &'static str;
+}
 
-    fn silu_inplace(self) -> Result<Self>;
+#[derive(Clone)]
+pub struct Tensor<D: TensorDevice> {
+    id: TensorID,
+    strider: TensorStrider,
+    device: Rc<RefCell<D>>,
+}
 
-    fn softmax_inplace(self, axis: usize) -> Result<Self>;
+impl<D: TensorDevice> Tensor<D> {
+    pub fn zeros(shape: Vec<usize>, device: Rc<RefCell<D>>) -> Result<Self> {
+        let strider: TensorStrider = TensorStrider::new(shape.clone());
+        let id = device
+            .borrow_mut()
+            .process_op(TensorDeviceOp::AllocTensor {
+                strider: strider.clone(),
+            })?
+            .unwrap();
+        Ok(Self {
+            id,
+            strider,
+            device,
+        })
+    }
 
-    fn rms_norm_inplace(self, eps: f32) -> Result<Self>;
+    pub fn shape(&self) -> &[usize] {
+        self.strider.shape()
+    }
 
-    fn rope_inplace(self, pos: usize, rope_dims: usize) -> Result<Self>;
+    pub fn len(&self) -> usize {
+        self.strider.len()
+    }
+
+    pub fn copy_from(&mut self, pos: &[usize], src: &Self) -> Result<()> {
+        self.device
+            .borrow_mut()
+            .process_op(TensorDeviceOp::CopyFrom {
+                dst: self.id,
+                pos: pos.to_vec(),
+                src: src.id,
+            })?;
+        Ok(())
+    }
+
+    pub fn view(self, shape: Vec<usize>) -> Result<Self> {
+        let strider = self.strider.view(shape)?;
+
+        self.device
+            .borrow_mut()
+            .process_op(TensorDeviceOp::EditTensor {
+                t: self.id,
+                strider: strider.clone(),
+            })?;
+        Ok(Self {
+            strider,
+            id: self.id,
+            device: self.device.clone(),
+        })
+    }
+
+    pub fn repeat(self, _n: usize, _axis: usize) -> Result<Self> {
+        todo!()
+    }
+
+    pub fn mul(self, _t: &Self) -> Result<Self> {
+        todo!()
+    }
+
+    pub fn add(self, _t: &Self) -> Result<Self> {
+        todo!()
+    }
+
+    pub fn matmul(&self, _t: &Self) -> Result<Self> {
+        todo!()
+    }
+}
+
+impl<D: TensorDevice> Drop for Tensor<D> {
+    fn drop(&mut self) {
+        self.device
+            .borrow_mut()
+            .process_op(TensorDeviceOp::RecycleTensor { t: self.id })
+            .unwrap();
+    }
 }

699946db770e7a50ee6f1642f08f80bc1b0c7c60
Li Yazhou <me.ssword@gmail.com>
2024-01-20 12:48:52 +0800

    add todo

--- crabml-core/src/tensor/tensor.rs
+++ crabml-core/src/tensor/tensor.rs
@@ -6,6 +6,7 @@ pub trait Tensor: Sized + Clone {
 
     /// alloc an owned tensor, only used on storing activations and kv caches.
     /// only F32 and F16 (not yet implemented) are supported.
+    /// TODO: add dtype parameter
     fn alloc(shape: &[usize], capacity: Option<usize>, device: Self::Device) -> Result<Self>;
 
     fn with_strider(self, strider: TensorStrider) -> Result<Self>;

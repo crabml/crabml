8ee8aa19805041d23c83af72e56a67f88fb365ec
Xuanwo <github@xuanwo.io>
2024-02-29 18:37:59 +0800

    Use only one device
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- crabml-core/src/lib.rs
+++ crabml-core/src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(portable_simd)]
 #![feature(slice_as_chunks)]
 #![feature(stdsimd)]
+#![feature(thread_local)]
+#![feature(lazy_cell)]
 
 #[allow(unreachable_patterns)]
 pub mod backends;

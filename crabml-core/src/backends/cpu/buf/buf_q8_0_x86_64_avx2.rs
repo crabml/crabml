1de7f9c0aae6a53f4c2fdd63e7e83b1857f8730b
Xuanwo <github@xuanwo.io>
2024-03-01 18:10:52 +0800

    inline some code
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- crabml-core/src/backends/cpu/buf/buf_q8_0_x86_64_avx2.rs
+++ crabml-core/src/backends/cpu/buf/buf_q8_0_x86_64_avx2.rs
@@ -80,6 +80,7 @@ pub fn vec_dot_q8_0_q8_0(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
 }
 
 /// TODO: Adding AVX-VNNI support so that we can use `_mm256_dpbssd_epi32`
+#[inline]
 unsafe fn mul_sum_i8_pairs_float(x: __m256i, y: __m256i) -> __m256 {
     // Get absolute values of x vectors
     let ax = _mm256_sign_epi8(x, x);
@@ -88,6 +89,7 @@ unsafe fn mul_sum_i8_pairs_float(x: __m256i, y: __m256i) -> __m256 {
     mul_sum_us8_pairs_float(ax, sy)
 }
 
+#[inline]
 unsafe fn mul_sum_us8_pairs_float(ax: __m256i, sy: __m256i) -> __m256 {
     let axl = _mm256_castsi256_si128(ax);
     let axh = _mm256_extractf128_si256(ax, 1);
@@ -99,6 +101,7 @@ unsafe fn mul_sum_us8_pairs_float(ax: __m256i, sy: __m256i) -> __m256 {
     sum_i16_pairs_float(doth, dotl)
 }
 
+#[inline]
 unsafe fn sum_i16_pairs_float(xh: __m128i, xl: __m128i) -> __m256 {
     let ones = _mm_set1_epi16(1);
     let summed_pairsl = _mm_madd_epi16(ones, xl);
@@ -107,7 +110,8 @@ unsafe fn sum_i16_pairs_float(xh: __m128i, xl: __m128i) -> __m256 {
     _mm256_cvtepi32_ps(summed_pairs)
 }
 
-// horizontally add 8 floats
+/// horizontally add 8 floats
+#[inline]
 unsafe fn hsum_float_8(x: __m256) -> f32 {
     let res = _mm256_extractf128_ps(x, 1);
     let res = _mm_add_ps(res, _mm256_castps256_ps128(x));

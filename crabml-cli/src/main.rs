9705a856c968fb8d35c62ffedb43bed5bd9f0cda
Li Yazhou <me.ssword@gmail.com>
2024-03-09 10:33:49 +0800

    buffer the writes

--- crabml-cli/src/main.rs
+++ crabml-cli/src/main.rs
@@ -1,5 +1,6 @@
 extern crate jemallocator;
 
+use std::io::BufWriter;
 use std::io::Write;
 use std::time::Instant;
 
@@ -89,31 +90,37 @@ fn main() -> Result<()> {
         println!("loaded model: {}ms", start_time.elapsed().as_millis());
     }
 
+    // it seems printing to stdout is not that fast, so we use a separate thread to keep the generation not blocked by the printing
+    let (tx, rx) = std::sync::mpsc::sync_channel(32);
+    let print_thread = std::thread::spawn(move || {
+        let mut step = 0;
+        let mut w = BufWriter::with_capacity(16, std::io::stdout().lock());
+        while let Ok(Some(token)) = rx.recv() {
+            write!(w, "{}", token).unwrap();
+            step += 1;
+            if step % 2 == 0 {
+                w.flush().unwrap();
+            }
+        }
+        w.flush().unwrap();
+    });
+
     let mut output = runner.generate(&args.prompt, args.steps, &mut sampler)?;
     print!("{}", &args.prompt);
 
     loop {
-        let token = {
-            let _t = metrics.total_walltime.track();
-            match output.next() {
-                Some(token) => token?,
-                None => break,
-            }
-        };
-
-        print!("{}", token);
-
-        if args.verbose {
-            print!("\nmetrics per token: ");
-            for (name, value) in metrics.as_vec().iter() {
-                println!("{}: {}ms", name, value);
+        let _t = metrics.total_walltime.track();
+        match output.next() {
+            Some(token) => tx.send(Some(token?)).unwrap(),
+            None => {
+                tx.send(None).unwrap();
+                break;
             }
         }
-
         metrics.reset();
-        std::io::stdout().flush().unwrap();
     }
 
+    print_thread.join().unwrap();
     println!();
     println!(
         "{} tokens/s, {} threads",

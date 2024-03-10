7379458930a1e107a5d4b02c7d332616d474aa3d
Chojan Shang <psiace@apache.org>
2024-03-10 16:10:06 +0800

    feat: support Q8_K (#90)
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- crabml-core/src/backends/cpu/buf/api.rs
+++ crabml-core/src/backends/cpu/buf/api.rs
@@ -11,6 +11,7 @@ use crate::backends::cpu::buf::buf_f16::vec_dot_f16_f16;
 use crate::backends::cpu::buf::QuantBufQ4_0;
 use crate::backends::cpu::buf::QuantBufQ4_1;
 use crate::backends::cpu::buf::QuantBufQ5_1;
+use crate::backends::cpu::buf::QuantBufQ8K;
 use crate::backends::cpu::buf::QuantBufQ8_0;
 use crate::backends::cpu::buf::QuantBufQ8_1;
 use crate::error::ErrorKind;
@@ -25,6 +26,7 @@ pub enum CpuTensorBuf<'a> {
     F16(Cow<'a, [f16]>),
     Q8_0(QuantBufQ8_0<'a>),
     Q8_1(QuantBufQ8_1<'a>),
+    Q8K(QuantBufQ8K<'a>),
     Q4_0(QuantBufQ4_0<'a>),
     Q4_1(QuantBufQ4_1<'a>),
     Q5_1(QuantBufQ5_1<'a>),
@@ -37,6 +39,7 @@ impl<'a> CpuTensorBuf<'a> {
             GGMLType::F16 => Ok(CpuTensorBuf::F16(f16_buf_from_bytes(buf))),
             GGMLType::Q8_0 => Ok(CpuTensorBuf::Q8_0(QuantBufQ8_0::from_bytes(buf))),
             GGMLType::Q8_1 => Ok(CpuTensorBuf::Q8_1(QuantBufQ8_1::from_bytes(buf))),
+            GGMLType::Q8K => Ok(CpuTensorBuf::Q8K(QuantBufQ8K::from_bytes(buf))),
             GGMLType::Q4_0 => Ok(CpuTensorBuf::Q4_0(QuantBufQ4_0::from_bytes(buf))),
             GGMLType::Q4_1 => Ok(CpuTensorBuf::Q4_1(QuantBufQ4_1::from_bytes(buf))),
             GGMLType::Q5_1 => Ok(CpuTensorBuf::Q5_1(QuantBufQ5_1::from_bytes(buf))),
@@ -61,6 +64,7 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::F16(buf) => buf.len(),
             CpuTensorBuf::Q8_0(buf) => buf.len(),
             CpuTensorBuf::Q8_1(buf) => buf.len(),
+            CpuTensorBuf::Q8K(buf) => buf.len(),
             CpuTensorBuf::Q4_0(buf) => buf.len(),
             CpuTensorBuf::Q4_1(buf) => buf.len(),
             CpuTensorBuf::Q5_1(buf) => buf.len(),
@@ -77,6 +81,7 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::F16(_) => GGMLType::F16,
             CpuTensorBuf::Q8_0(_) => GGMLType::Q8_0,
             CpuTensorBuf::Q8_1(_) => GGMLType::Q8_1,
+            CpuTensorBuf::Q8K(_) => GGMLType::Q8K,
             CpuTensorBuf::Q4_0(_) => GGMLType::Q4_0,
             CpuTensorBuf::Q4_1(_) => GGMLType::Q4_1,
             CpuTensorBuf::Q5_1(_) => GGMLType::Q5_1,
@@ -89,6 +94,7 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::F16(_) => GGMLType::F16,
             CpuTensorBuf::Q8_0(_) => GGMLType::Q8_0,
             CpuTensorBuf::Q8_1(_) => GGMLType::Q8_1,
+            CpuTensorBuf::Q8K(_) => GGMLType::Q8K,
             CpuTensorBuf::Q4_0(_) => GGMLType::Q8_0,
             CpuTensorBuf::Q4_1(_) => GGMLType::Q8_1,
             CpuTensorBuf::Q5_1(_) => GGMLType::Q8_1,
@@ -113,6 +119,7 @@ impl<'a> CpuTensorBuf<'a> {
                 CpuTensorBuf::F16(buf) => dequantize_f16_buf(&buf, 0).collect(),
                 CpuTensorBuf::Q8_0(buf) => buf.dequantize(0).collect(),
                 CpuTensorBuf::Q8_1(buf) => buf.dequantize(0).collect(),
+                CpuTensorBuf::Q8K(buf) => buf.dequantize(0).collect(),
                 CpuTensorBuf::Q4_0(buf) => buf.dequantize(0).collect(),
                 CpuTensorBuf::Q4_1(buf) => buf.dequantize(0).collect(),
                 CpuTensorBuf::Q5_1(buf) => buf.dequantize(0).collect(),
@@ -132,6 +139,7 @@ impl<'a> CpuTensorBuf<'a> {
             GGMLType::Q8_1 => Ok(CpuTensorBuf::Q8_1(QuantBufQ8_1::quantize(
                 self.as_f32_ref(),
             ))),
+            GGMLType::Q8K => Ok(CpuTensorBuf::Q8K(QuantBufQ8K::quantize(self.as_f32_ref()))),
             GGMLType::Q4_0 => Ok(CpuTensorBuf::Q4_0(QuantBufQ4_0::quantize(
                 self.as_f32_ref(),
             ))),
@@ -156,6 +164,7 @@ impl<'a> CpuTensorBuf<'a> {
             (F16(a), F16(b)) => vec_dot_f16_f16(a, a_offset, b, b_offset, len),
             (Q8_0(a), Q8_0(b)) => a.vec_dot(a_offset, b, b_offset, len),
             (Q8_1(a), Q8_1(b)) => a.vec_dot(a_offset, b, b_offset, len),
+            (Q8K(a), Q8K(b)) => a.vec_dot(a_offset, b, b_offset, len),
             (Q4_0(a), Q8_0(b)) => a.vec_dot(a_offset, b, b_offset, len),
             (Q4_1(a), Q8_1(b)) => a.vec_dot(a_offset, b, b_offset, len),
             (Q5_1(a), Q8_1(b)) => a.vec_dot(a_offset, b, b_offset, len),
@@ -186,6 +195,7 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::F16(buf) => Box::new(dequantize_f16_buf(buf, offset).take(len)),
             CpuTensorBuf::Q8_0(buf) => Box::new(buf.dequantize(offset).take(len)),
             CpuTensorBuf::Q8_1(buf) => Box::new(buf.dequantize(offset).take(len)),
+            CpuTensorBuf::Q8K(buf) => Box::new(buf.dequantize(offset).take(len)),
             CpuTensorBuf::Q4_0(buf) => Box::new(buf.dequantize(offset).take(len)),
             CpuTensorBuf::Q4_1(buf) => Box::new(buf.dequantize(offset).take(len)),
             CpuTensorBuf::Q5_1(buf) => Box::new(buf.dequantize(offset).take(len)),
@@ -241,6 +251,7 @@ impl Clone for CpuTensorBuf<'_> {
             CpuTensorBuf::F16(buf) => Self::F16(buf.clone()),
             CpuTensorBuf::Q8_0(buf) => Self::Q8_0(buf.clone()),
             CpuTensorBuf::Q8_1(buf) => Self::Q8_1(buf.clone()),
+            CpuTensorBuf::Q8K(buf) => Self::Q8K(buf.clone()),
             CpuTensorBuf::Q4_0(buf) => Self::Q4_0(buf.clone()),
             CpuTensorBuf::Q4_1(buf) => Self::Q4_1(buf.clone()),
             CpuTensorBuf::Q5_1(buf) => Self::Q5_1(buf.clone()),
---
+++ crabml-core/src/backends/cpu/buf/buf_q8_k.rs
@@ -0,0 +1,211 @@
+use std::borrow::Cow;
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct BlockQ8K {
+    d: f32,        // delta
+    qs: [i8; 256], // quants
+    bsums: [i16; 16],
+}
+
+impl BlockQ8K {
+    pub fn dequantize(&self, buf: &mut [f32]) {
+        let d = self.d;
+        for (i, &q) in self.qs.iter().enumerate() {
+            buf[i] = d * q as f32
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct QuantBufQ8K<'a> {
+    pub blocks: Cow<'a, [BlockQ8K]>,
+}
+
+impl<'a> QuantBufQ8K<'_> {
+    pub fn from_bytes(data: &'a [u8]) -> Self {
+        let blk_size = std::mem::size_of::<BlockQ8K>();
+        assert_eq!(
+            data.len() % blk_size,
+            0,
+            "data length must be a multiple of QuantBlockQ8_K size"
+        );
+        let blocks = unsafe {
+            std::slice::from_raw_parts(data.as_ptr() as *const BlockQ8K, data.len() / blk_size)
+        };
+        Self {
+            blocks: blocks.into(),
+        }
+    }
+    pub fn quantize(data: &[f32]) -> Self {
+        let bs = quantize_f32_q8_k(data);
+        Self { blocks: bs.into() }
+    }
+
+    fn blocks(&self) -> &[BlockQ8K] {
+        &self.blocks
+    }
+
+    pub fn len(&self) -> usize {
+        self.blocks.len() * 32
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.blocks.is_empty()
+    }
+
+    pub fn dequantize(&'a self, start: usize) -> impl Iterator<Item = f32> + 'a {
+        assert!(start % 32 == 0);
+
+        let block_start = start / 32;
+        self.blocks()[block_start..].iter().flat_map(|blk| {
+            let mut buf = [0f32; 256];
+            blk.dequantize(&mut buf);
+            buf.into_iter()
+        })
+    }
+
+    pub fn vec_dot(&self, a_offset: usize, b: &QuantBufQ8K, b_offset: usize, len: usize) -> f32 {
+        let abs = &self.blocks[a_offset / 32..(a_offset + len) / 32];
+        let bbs = &b.blocks[b_offset / 32..(b_offset + len) / 32];
+
+        vec_dot_q8_k_q8_k(abs, bbs)
+    }
+}
+
+mod impl_fallback {
+    use super::BlockQ8K;
+    pub fn quantize_f32_q8_k(data: &[f32]) -> Vec<BlockQ8K> {
+        let mut bs = Vec::with_capacity(data.len() / 32);
+
+        for chunk in data.chunks(256) {
+            let mut max_abs_value = 0.0;
+            let mut max_value = 0.0;
+
+            // Find the maximum absolute value in the chunk
+            for &value in chunk {
+                let abs_value = value.abs();
+                if abs_value > max_abs_value {
+                    max_abs_value = abs_value;
+                    max_value = value;
+                }
+            }
+
+            let scale = -128f32 / max_value;
+            let mut d = 1.0 / scale; // Compute the scaling factor
+            let mut qs = [0_i8; 256]; // Initialize the quantized values array
+            let mut bsums = [0_i16; 16];
+
+            // Quantize the chunk
+
+            if max_abs_value == 0f32 {
+                d = 0f32;
+                qs.fill(0)
+            } else {
+                for (i, q) in qs.iter_mut().enumerate() {
+                    // ggml uses nearest_int with bit magic here, maybe we want the same
+                    // but we would have to test and benchmark it.
+                    let v = (scale * chunk[i]).round();
+                    *q = v.min(127.) as i8
+                }
+                for i in 0..16 {
+                    let mut sum = 0i32;
+                    for j in 0..16 {
+                        sum += qs[i * 16 + j] as i32
+                    }
+                    bsums[i] = sum as i16
+                }
+            }
+
+            // Store the block with the scaling factor, quantized values
+            bs.push(BlockQ8K { d, qs, bsums });
+        }
+
+        bs
+    }
+
+    pub fn vec_dot_q8_k_q8_k(abs: &[BlockQ8K], bbs: &[BlockQ8K]) -> f32 {
+        let mut sumf = 0f32;
+        for (abs, bbs) in abs.iter().zip(bbs.iter()) {
+            let sum_i = abs
+                .qs
+                .iter()
+                .zip(bbs.qs.iter())
+                .map(|(&x, &y)| x as i32 * y as i32)
+                .sum::<i32>();
+            sumf += sum_i as f32 * abs.d * bbs.d
+        }
+
+        sumf
+    }
+}
+use impl_fallback::*;
+
+#[cfg(test)]
+mod tests {
+    use half::f16;
+
+    use super::*;
+
+    #[test]
+    fn test_q8_k_block() {
+        assert_eq!(
+            std::mem::size_of::<BlockQ8K>(),
+            std::mem::size_of::<f32>() + 256 + 16 * 2,
+            "wrong q8_k block size/padding"
+        );
+
+        let mut buf: [u8; 292] = [0x1; 292];
+        let delta = f16::from_f32(3.0).to_bits().to_le_bytes();
+        let min_val = f16::from_f32(1.0).to_bits().to_le_bytes();
+
+        buf[0] = delta[0];
+        buf[1] = delta[1];
+        buf[2] = min_val[0];
+        buf[3] = min_val[1];
+        buf[4] = 2;
+        buf[5] = 3;
+        buf[6] = 4;
+        buf[4 + 15] = 7;
+        buf[283] = 10;
+
+        let blocks = QuantBufQ8K::from_bytes(&buf[0..292]).blocks;
+        assert_eq!(blocks[0].d, 0.007828236);
+        assert_eq!(blocks[0].qs[0..16], [
+            2, 3, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7
+        ]);
+        assert_eq!(blocks[0].bsums[0..16], [
+            257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 2561, 257, 257, 257, 257
+        ]);
+    }
+
+    #[test]
+    fn test_q8_k_quantize() {
+        let data = vec![
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+            -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
+        ];
+        let bs = QuantBufQ8K::quantize(&data);
+
+        assert_eq!(bs.blocks.len(), 1);
+        assert_eq!(bs.blocks[0].d, 0.0625);
+
+        let mut dequantize = [0.0f32; 256];
+        bs.blocks[0].dequantize(&mut dequantize);
+        assert_eq!(dequantize, *data);
+    }
+}

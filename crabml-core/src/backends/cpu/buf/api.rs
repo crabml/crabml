14d2075c97520e690292c9b4dcb92ad32f084807
Chojan Shang <psiace@apache.org>
2024-03-12 12:54:05 +0800

    feat: add support for Q6k (#98)
    * feat: support Q6_K
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * feat: minor fix
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * refactor: remove the code not impl by me
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * chore: make fmt happy
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * fix: made q6k work as expected
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * refactor: minor rename
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * test: minor fix
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    * chore: minor fmt
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    
    ---------
    
    Signed-off-by: Chojan Shang <psiace@apache.org>
    Co-authored-by: flaneur <me.ssword@gmail.com>

--- crabml-core/src/backends/cpu/buf/api.rs
+++ crabml-core/src/backends/cpu/buf/api.rs
@@ -13,6 +13,7 @@ use crate::backends::cpu::buf::QuantBufQ4_0;
 use crate::backends::cpu::buf::QuantBufQ4_1;
 use crate::backends::cpu::buf::QuantBufQ5_0;
 use crate::backends::cpu::buf::QuantBufQ5_1;
+use crate::backends::cpu::buf::QuantBufQ6K;
 use crate::backends::cpu::buf::QuantBufQ8K;
 use crate::backends::cpu::buf::QuantBufQ8_0;
 use crate::backends::cpu::buf::QuantBufQ8_1;
@@ -34,6 +35,7 @@ pub enum CpuTensorBuf<'a> {
     Q4_1(QuantBufQ4_1<'a>),
     Q5_0(QuantBufQ5_0<'a>),
     Q5_1(QuantBufQ5_1<'a>),
+    Q6K(QuantBufQ6K<'a>),
 }
 
 impl<'a> CpuTensorBuf<'a> {
@@ -49,6 +51,7 @@ impl<'a> CpuTensorBuf<'a> {
             GGMLType::Q4_1 => Ok(CpuTensorBuf::Q4_1(QuantBufQ4_1::from_bytes(buf))),
             GGMLType::Q5_0 => Ok(CpuTensorBuf::Q5_0(QuantBufQ5_0::from_bytes(buf))),
             GGMLType::Q5_1 => Ok(CpuTensorBuf::Q5_1(QuantBufQ5_1::from_bytes(buf))),
+            GGMLType::Q6K => Ok(CpuTensorBuf::Q6K(QuantBufQ6K::from_bytes(buf))),
             _ => unimplemented!(),
         }
     }
@@ -76,6 +79,8 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::Q5_1(buf) => buf.len(),
             CpuTensorBuf::Q4_0(buf) => buf.len(),
             CpuTensorBuf::Q4_1(buf) => buf.len(),
+            CpuTensorBuf::Q5_1(buf) => buf.len(),
+            CpuTensorBuf::Q6K(buf) => buf.len(),
         }
     }
 
@@ -95,6 +100,7 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::Q4_1(_) => GGMLType::Q4_1,
             CpuTensorBuf::Q5_0(_) => GGMLType::Q5_0,
             CpuTensorBuf::Q5_1(_) => GGMLType::Q5_1,
+            CpuTensorBuf::Q6K(_) => GGMLType::Q6K,
         }
     }
 
@@ -110,6 +116,8 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::Q5_1(_) => GGMLType::Q8_1,
             CpuTensorBuf::Q4_0(_) => GGMLType::Q8_0,
             CpuTensorBuf::Q4_1(_) => GGMLType::Q8_1,
+            CpuTensorBuf::Q5_1(_) => GGMLType::Q8_1,
+            CpuTensorBuf::Q6K(_) => GGMLType::Q8K,
         }
     }
 
@@ -137,6 +145,7 @@ impl<'a> CpuTensorBuf<'a> {
                 CpuTensorBuf::Q4_1(buf) => buf.dequantize(0).collect(),
                 CpuTensorBuf::Q5_0(buf) => buf.dequantize(0).collect(),
                 CpuTensorBuf::Q5_1(buf) => buf.dequantize(0).collect(),
+                CpuTensorBuf::Q6K(buf) => buf.dequantize(0).collect(),
             })),
             GGMLType::F16 => unimplemented!(),
             _ => unreachable!(),
@@ -167,6 +176,7 @@ impl<'a> CpuTensorBuf<'a> {
             GGMLType::Q5_0 => Ok(CpuTensorBuf::Q5_0(QuantBufQ5_0::quantize(
                 self.as_f32_ref(),
             ))),
+            GGMLType::Q6K => Ok(CpuTensorBuf::Q6K(QuantBufQ6K::quantize(self.as_f32_ref()))),
             _ => Err((
                 ErrorKind::TensorError,
                 format!("quantize to {:?} is not supported", dtype),
@@ -188,6 +198,7 @@ impl<'a> CpuTensorBuf<'a> {
             (Q4_1(a), Q8_1(b)) => a.vec_dot(a_offset, b, b_offset, len),
             (Q5_0(a), Q8_0(b)) => a.vec_dot(a_offset, b, b_offset, len),
             (Q5_1(a), Q8_1(b)) => a.vec_dot(a_offset, b, b_offset, len),
+            (Q6K(a), Q8K(b)) => a.vec_dot(a_offset, b, b_offset, len),
             _ => unreachable!(),
         }
     }
@@ -221,6 +232,7 @@ impl<'a> CpuTensorBuf<'a> {
             CpuTensorBuf::Q4_1(buf) => Box::new(buf.dequantize(offset).take(len)),
             CpuTensorBuf::Q5_0(buf) => Box::new(buf.dequantize(offset).take(len)),
             CpuTensorBuf::Q5_1(buf) => Box::new(buf.dequantize(offset).take(len)),
+            CpuTensorBuf::Q6K(buf) => Box::new(buf.dequantize(offset).take(len)),
         };
 
         match self {
@@ -279,6 +291,8 @@ impl Clone for CpuTensorBuf<'_> {
             CpuTensorBuf::Q5_1(buf) => Self::Q5_1(buf.clone()),
             CpuTensorBuf::Q4_0(buf) => Self::Q4_0(buf.clone()),
             CpuTensorBuf::Q4_1(buf) => Self::Q4_1(buf.clone()),
+            CpuTensorBuf::Q5_1(buf) => Self::Q5_1(buf.clone()),
+            CpuTensorBuf::Q6K(buf) => Self::Q6K(buf.clone()),
         }
     }
 }
--- crabml-core/src/backends/cpu/buf/util.rs
+++ crabml-core/src/backends/cpu/buf/util.rs
@@ -15,6 +15,131 @@ pub fn nearest_i32(fval: f32) -> i32 {
     (i & 0x007fffff) - 0x00400000
 }
 
+pub fn make_qx_quants(n: usize, nmax: i32, data: &[f32], ls: &mut [i8], rmse_type: i32) -> f32 {
+    let mut max = 0.0;
+    let mut abs_max = 0.0;
+    for &x in data.iter().take(n) {
+        let abs_x = x.abs();
+        if abs_x > abs_max {
+            abs_max = abs_x;
+            max = x;
+        }
+    }
+    if abs_max == 0. {
+        // all zero
+        for l in ls.iter_mut().take(n) {
+            *l = 0;
+        }
+        return 0.;
+    }
+    let mut iscale = -(nmax as f32) / max;
+    if rmse_type == 0 {
+        for (i, &xi) in data.iter().take(n).enumerate() {
+            let l = nearest_i32(iscale * xi);
+            ls[i] = (nmax + l.clamp(-nmax, nmax - 1)) as i8;
+        }
+        return 1.0 / iscale;
+    }
+    let weight_type = rmse_type % 2;
+    let mut sumlx = 0f32;
+    let mut suml2 = 0f32;
+    for (i, &xi) in data.iter().take(n).enumerate() {
+        let l = nearest_i32(iscale * xi);
+        let l = l.clamp(-nmax, nmax - 1);
+        ls[i] = (l + nmax) as i8;
+        let w = if weight_type == 1 { xi * xi } else { 1.0 };
+        let l = l as f32;
+        sumlx += w * xi * l;
+        suml2 += w * l * l;
+    }
+    let mut scale = sumlx / suml2;
+    let mut best = scale * sumlx;
+    for _itry in 0..3 {
+        let iscale = 1.0 / scale;
+        let mut slx = 0f32;
+        let mut sl2 = 0f32;
+        let mut changed = false;
+        for (i, &xi) in data.iter().take(n).enumerate() {
+            let l = nearest_i32(iscale * xi);
+            let l = l.clamp(-nmax, nmax - 1);
+            if l + nmax != ls[i] as i32 {
+                changed = true;
+            }
+            let w = if weight_type == 1 { xi * xi } else { 1f32 };
+            let l = l as f32;
+            slx += w * xi * l;
+            sl2 += w * l * l;
+        }
+        if !changed || sl2 == 0.0 || slx * slx <= best * sl2 {
+            break;
+        }
+        for (i, &xi) in data.iter().take(n).enumerate() {
+            let l = nearest_i32(iscale * xi);
+            ls[i] = (nmax + l.clamp(-nmax, nmax - 1)) as i8;
+        }
+        sumlx = slx;
+        suml2 = sl2;
+        scale = sumlx / suml2;
+        best = scale * sumlx;
+    }
+    for _itry in 0..5 {
+        let mut n_changed = 0;
+        for (i, &xi) in data.iter().take(n).enumerate() {
+            let w = if weight_type == 1 { xi * xi } else { 1. };
+            let l = ls[i] as i32 - nmax;
+            let mut slx = sumlx - w * xi * l as f32;
+            if slx > 0. {
+                let mut sl2 = suml2 - w * l as f32 * l as f32;
+                let new_l = nearest_i32(xi * sl2 / slx);
+                let new_l = new_l.clamp(-nmax, nmax - 1);
+                if new_l != l {
+                    slx += w * xi * new_l as f32;
+                    sl2 += w * new_l as f32 * new_l as f32;
+                    if sl2 > 0. && slx * slx * suml2 > sumlx * sumlx * sl2 {
+                        ls[i] = (nmax + new_l) as i8;
+                        sumlx = slx;
+                        suml2 = sl2;
+                        scale = sumlx / suml2;
+                        best = scale * sumlx;
+                        n_changed += 1;
+                    }
+                }
+            }
+        }
+        if n_changed == 0 {
+            break;
+        }
+    }
+    if rmse_type < 3 {
+        return scale;
+    }
+    for is in -4..4 {
+        if is == 0 {
+            continue;
+        }
+        iscale = -(nmax as f32 + 0.1f32 * is as f32) / max;
+        let mut sumlx = 0.;
+        let mut suml2 = 0.;
+        for &xi in data.iter().take(n) {
+            let l = nearest_i32(iscale * xi);
+            let l = l.clamp(-nmax, nmax - 1);
+            let w = if weight_type == 1 { xi * xi } else { 1. };
+            let l = l as f32;
+            sumlx += w * xi * l;
+            suml2 += w * l * l;
+        }
+        if suml2 > 0. && sumlx * sumlx > best * suml2 {
+            for (i, &xi) in data.iter().take(n).enumerate() {
+                let l = nearest_i32(iscale * xi);
+                ls[i] = (nmax + l.clamp(-nmax, nmax - 1)) as i8;
+            }
+            scale = sumlx / suml2;
+            best = scale * sumlx;
+        }
+    }
+    scale
+}
+
 pub fn make_qkx1_quants(
     n: usize,
     nmax: i32,

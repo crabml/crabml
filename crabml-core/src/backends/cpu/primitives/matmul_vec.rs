744c9a1ddf50384481622c5c495d85c4ae070771
Li Yazhou <me.ssword@gmail.com>
2024-03-22 22:59:39 +0800

    simplify matmul

--- crabml-core/src/backends/cpu/primitives/matmul_vec.rs
+++ crabml-core/src/backends/cpu/primitives/matmul_vec.rs
@@ -19,39 +19,10 @@ pub fn matmul_vec<'a>(
     assert!(strider2.shape().len() == 1);
     assert!(strider1.shape()[1] == strider2.shape()[0]);
     assert!(strider2.is_contiguous());
+    assert!(bufc.len() % 4 == 0);
 
-    // if the input is contiguous, we can use SIMD to accelerate the computation
-    if strider1.is_contiguous() && bufa.len() % 32 == 0 {
-        gemv_simd(device, bufa, bufb, bufc);
-        return Ok(());
-    }
-
-    // fall back to the naive implementation if stride1 is not contiguous
-    gemv_naive_f32(bufa, bufb, bufc, strider1);
+    gemv_simd(device, bufa, bufb, bufc);
     Ok(())
-}
-
-fn gemv_naive_f32<'a>(
-    bufa: &CpuTensorBuf<'a>,
-    bufb: &CpuTensorBuf<'a>,
-    bufc: &mut CpuTensorBuf<'a>,
-    strider1: &TensorStrider,
-) {
-    let c = bufc.as_f32_mut();
-    let a = bufa.as_f32_ref();
-    let b = bufb.as_f32_ref();
-    let m_stride = strider1.strides()[0];
-    let k_stride = strider1.strides()[1];
-    let m = strider1.shape()[0];
-    let k = strider1.shape()[1];
-
-    for mi in 0..m {
-        let mut sum = 0.0;
-        for ki in 0..k {
-            sum += a[mi * m_stride + ki * k_stride] * b[ki];
-        }
-        c[mi] = sum;
-    }
 }
 
 fn gemv_simd<'a>(
@@ -60,7 +31,6 @@ fn gemv_simd<'a>(
     bufb: &CpuTensorBuf<'a>,
     bufc: &mut CpuTensorBuf<'a>,
 ) {
-    assert!(bufa.len() % 32 == 0);
     let metrics = device.metrics().clone();
 
     let bufc = bufc.as_f32_mut();

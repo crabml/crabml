fddea136676f47445b9588158db03bb690ae2760
Li Yazhou <me.ssword@gmail.com>
2024-03-25 11:59:09 +0800

    remove save_kv_cache_walltime

--- crabml-core/src/backends/cpu/primitives/silu.rs
+++ crabml-core/src/backends/cpu/primitives/silu.rs
@@ -4,7 +4,7 @@ use crate::backends::cpu::CpuTensorDeviceRef;
 use crate::error::Result;
 
 pub fn silu_inplace<'a>(device: CpuTensorDeviceRef<'a>, buf: &mut CpuTensorBuf<'a>) -> Result<()> {
-    let exp_cache = &device.exp_cache;
+    let exp_cache = device.exp_cache.as_ref();
     buf.iter_f32_mut().for_each(|n| {
         let nexp = exp_f32_cached(-*n, exp_cache);
         *n /= 1.0 + nexp

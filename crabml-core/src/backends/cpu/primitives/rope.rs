064f46ee30374fc1a689ae1e1134bc766135b767
Li Yazhou <me.ssword@gmail.com>
2024-03-25 11:55:46 +0800

    tune rope

--- crabml-core/src/backends/cpu/primitives/rope.rs
+++ crabml-core/src/backends/cpu/primitives/rope.rs
@@ -48,15 +48,15 @@ fn rope_llama(buf: &mut [f32], pos: usize, head_dim: usize, rope_dim: usize) {
     let theta_scale = 10000_f32.powf(-2.0 / head_dim as f32);
     buf.chunks_exact_mut(head_dim).for_each(|chunk| {
         let mut theta: f32 = pos as f32;
-        for i in 0..rope_dim / 2 {
+        for i in (0..rope_dim).step_by(2) {
             let cos_theta = theta.cos();
             let sin_theta = theta.sin();
             theta *= theta_scale;
             unsafe {
-                let qp0 = *chunk.get_unchecked(i * 2);
-                let qp1 = *chunk.get_unchecked(i * 2 + 1);
-                *chunk.get_unchecked_mut(i * 2) = qp0 * cos_theta - qp1 * sin_theta;
-                *chunk.get_unchecked_mut(i * 2 + 1) = qp0 * sin_theta + qp1 * cos_theta;
+                let qp0 = *chunk.get_unchecked(i);
+                let qp1 = *chunk.get_unchecked(i + 1);
+                *chunk.get_unchecked_mut(i) = qp0 * cos_theta - qp1 * sin_theta;
+                *chunk.get_unchecked_mut(i + 1) = qp0 * sin_theta + qp1 * cos_theta;
             }
         }
     });

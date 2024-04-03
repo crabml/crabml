84d4b377a78c2e4112eaa15a7965b0bdd2dc7e37
Li Yazhou <me.ssword@gmail.com>
2024-04-03 22:00:32 +0800

    simplify quantize simd

--- crabml-core/src/backends/cpu/buf/buf_q8_0.rs
+++ crabml-core/src/backends/cpu/buf/buf_q8_0.rs
@@ -134,53 +134,6 @@ mod impl_aarch64_neon {
 
     use super::BlockQ8_0;
 
-    pub fn quantize_f32_q8_0(data: &[f32]) -> Vec<BlockQ8_0> {
-        let mut bs = Vec::with_capacity(data.len() / 32);
-
-        unsafe {
-            for i in (0..data.len()).step_by(32) {
-                let mut vsrc = [aarch64::vdupq_n_f32(0.0); 8];
-                let mut vasrc = [aarch64::vdupq_n_f32(0.0); 8];
-                let mut vmax = [aarch64::vdupq_n_f32(0.0); 8];
-
-                for j in 0..8 {
-                    vsrc[j] = aarch64::vld1q_f32(data.as_ptr().add(i + j * 4));
-                    vasrc[j] = aarch64::vabsq_f32(vsrc[j]);
-                }
-
-                for j in 0..4 {
-                    vmax[2 * j] = aarch64::vmaxq_f32(vasrc[2 * j], vasrc[2 * j + 1]);
-                }
-                for j in 0..2 {
-                    vmax[4 * j] = aarch64::vmaxq_f32(vmax[4 * j], vmax[4 * j + 2]);
-                }
-                for j in 0..1 {
-                    vmax[8 * j] = aarch64::vmaxq_f32(vmax[8 * j], vmax[8 * j + 4]);
-                }
-                let max = aarch64::vmaxvq_f32(vmax[0]);
-
-                let d = max / 127.0;
-                let mut qs = [0_i8; 32];
-
-                for j in 0..8 {
-                    let v = aarch64::vdivq_f32(vsrc[j], aarch64::vdupq_n_f32(d));
-                    let vi = aarch64::vcvtq_s32_f32(v);
-                    qs[4 * j] = aarch64::vgetq_lane_s32(vi, 0) as i8;
-                    qs[4 * j + 1] = aarch64::vgetq_lane_s32(vi, 1) as i8;
-                    qs[4 * j + 2] = aarch64::vgetq_lane_s32(vi, 2) as i8;
-                    qs[4 * j + 3] = aarch64::vgetq_lane_s32(vi, 3) as i8;
-                }
-
-                bs.push(BlockQ8_0 {
-                    d: f16::from_f32(d),
-                    qs,
-                });
-            }
-        }
-
-        bs
-    }
-
     pub fn vec_dot_q8_0_q8_0(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
         assert!(abs.len() == bbs.len());
 
@@ -283,63 +236,6 @@ mod impl_x86_64_avx2 {
     use super::BlockQ8_0;
 
     /// Inspired a lot by ggml https://github.com/ggerganov/ggml/blob/master/src/ggml-quants.c
-
-    pub fn quantize_f32_q8_0(data: &[f32]) -> Vec<BlockQ8_0> {
-        debug_assert_eq!(data.len() % 32, 0);
-
-        let mut bs = Vec::with_capacity(data.len() / 32);
-
-        unsafe {
-            let mask = _mm256_set1_ps(-0.0);
-
-            for chunk in data.chunks(32) {
-                let mut max_abs_values = _mm256_setzero_ps();
-
-                for values in chunk.chunks(8) {
-                    let value_vec = _mm256_loadu_ps(values.as_ptr());
-                    max_abs_values =
-                        _mm256_max_ps(max_abs_values, _mm256_andnot_ps(mask, value_vec))
-                }
-
-                let max_abs_value = {
-                    let mut max_vals = [0.0; 8];
-                    _mm256_storeu_ps(max_vals.as_mut_ptr(), max_abs_values);
-                    *max_vals
-                        .iter()
-                        .max_by(|x, y| x.partial_cmp(y).unwrap_unchecked())
-                        .unwrap_unchecked()
-                };
-
-                let d = max_abs_value / 127.0;
-                let d_vec = _mm256_set1_ps(d);
-                let mut qs = [0_i8; 32];
-                let mut temp = [0i32; 8]; // Temporary array to hold intermediate results
-
-                for (chunk_index, values) in chunk.chunks(8).enumerate() {
-                    let values_vec = _mm256_loadu_ps(values.as_ptr());
-                    let scaled_vec = _mm256_div_ps(values_vec, d_vec);
-                    let clamped_vec = _mm256_max_ps(
-                        _mm256_set1_ps(i8::MIN as f32),
-                        _mm256_min_ps(_mm256_set1_ps(i8::MAX as f32), scaled_vec),
-                    );
-                    let converted_vec = _mm256_cvtps_epi32(clamped_vec);
-                    _mm256_storeu_si256(temp.as_mut_ptr() as *mut __m256i, converted_vec);
-
-                    for (i, &value) in temp.iter().enumerate() {
-                        qs[chunk_index * 8 + i] = value as i8;
-                    }
-                }
-
-                bs.push(BlockQ8_0 {
-                    d: f16::from_f32(d),
-                    qs,
-                });
-            }
-        }
-
-        bs
-    }
-
     pub fn vec_dot_q8_0_q8_0(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
         debug_assert_eq!(abs.len(), bbs.len());
 
@@ -434,40 +330,6 @@ mod impl_fallback {
     use half::f16;
 
     use super::BlockQ8_0;
-
-    pub fn quantize_f32_q8_0(data: &[f32]) -> Vec<BlockQ8_0> {
-        let mut bs = Vec::with_capacity(data.len() / 32);
-
-        for chunk in data.chunks(32) {
-            let mut max_abs_value = 0.0;
-
-            // Find the maximum absolute value in the chunk
-            for &value in chunk {
-                let abs_value = value.abs();
-                if abs_value > max_abs_value {
-                    max_abs_value = abs_value;
-                }
-            }
-
-            let d = max_abs_value / 127.0; // Compute the scaling factor
-            let mut qs = [0_i8; 32]; // Initialize the quantized values array
-
-            // Quantize the chunk
-            for (i, &value) in chunk.iter().enumerate() {
-                let scaled_value = value / d; // Scale the value
-                // Convert the scaled value to i8, clamping it to the i8 range
-                qs[i] = scaled_value.max(i8::MIN as f32).min(i8::MAX as f32) as i8;
-            }
-
-            // Store the block with the scaling factor and quantized values
-            bs.push(BlockQ8_0 {
-                d: f16::from_f32(d),
-                qs,
-            });
-        }
-
-        bs
-    }
 
     pub fn vec_dot_q8_0_q8_0(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
         let mut sumf: f32 = 0.0;

4cbd5a15507d37a370bf9e0e138a322ad2b35a11
baishen <baishen2009@gmail.com>
2024-04-09 23:58:46 +0800

    optimize(quants): simd optimization on neon over q4_1

--- crabml-core/src/backends/cpu/buf/buf_q4_1.rs
+++ crabml-core/src/backends/cpu/buf/buf_q4_1.rs
@@ -117,6 +117,92 @@ pub fn quantize_f32_q4_1(data: &[f32]) -> Vec<BlockQ4_1> {
 }
 
 pub fn vec_dot_q4_1_q8_1(abs: &[BlockQ4_1], bbs: &[BlockQ8_1]) -> f32 {
+    #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+    {
+        vec_dot_q4_1_q8_1_neon(abs, bbs)
+    }
+
+    #[cfg(not(all(target_arch = "aarch64", target_feature = "neon")))]
+    {
+        vec_dot_q4_1_q8_1_fallback(abs, bbs)
+    }
+}
+
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+pub fn vec_dot_q4_1_q8_1_neon(abs: &[BlockQ4_1], bbs: &[BlockQ8_1]) -> f32 {
+    use std::arch::aarch64::*;
+    let n_blocks = abs.len();
+    let n_blocks_rounded = n_blocks - n_blocks % 2;
+
+    let mut sumf = unsafe {
+        let mut sumv0 = vdupq_n_f32(0.0);
+        let mut sumv1 = vdupq_n_f32(0.0);
+        let mut summs = 0;
+        let zerov = vdupq_n_s32(0);
+
+        for i in (0..n_blocks_rounded).step_by(2) {
+            let ab0 = abs.get_unchecked(i);
+            let ab1 = abs.get_unchecked(i + 1);
+            let bb0 = bbs.get_unchecked(i);
+            let bb1 = bbs.get_unchecked(i + 1);
+
+            summs +=
+                f16::to_f32(ab0.m) * f16::to_f32(bb0.s) + f16::to_f32(ab1.m) * f16::to_f32(bb1.s);
+
+            let m4b = vdupq_n_u8(0x0F);
+
+            let v0_0 = vdu1q_u8(ab0.qs);
+            let v0_1 = vdu1q_u8(ab1.qs);
+
+            // 4-bit -> 8-bit
+            let v0_0l = vreinterpretq_s8_u8(vandq_u8(v0_0, m4b));
+            let v0_0h = vreinterpretq_s8_u8(vshrq_n_u8(v0_0, 4));
+            let v0_1l = vreinterpretq_s8_u8(vandq_u8(v0_1, m4b));
+            let v0_1h = vreinterpretq_s8_u8(vshrq_n_u8(v0_1, 4));
+
+            // load y
+            let v1_0l = v1d1q_s8(bb0.qs.as_ptr());
+            let v1_0h = v1d1q_s8(bb0.qs.as_prt().add(16));
+            let v1_1l = v1d1q_s8(bb1.qs.as_ptr());
+            let v1_1h = v1d1q_s8(bb1.qs.as_prt().add(16));
+
+            // dot product into int32x4_t
+            sumv0 = vmlaq_n_f32(
+                sumv0,
+                vcvtq_f32_s32(vaddq_s32(
+                    vdotq_s32(zerov, v0_0l, v1_0l),
+                    vdotq_s32(zerov, v0_0h, v1_0h),
+                )),
+                f16::to_f32(ab0.d) * f16::to_f32(bb0.d),
+            );
+            sumv1 = vmlaq_n_f32(
+                sumv1,
+                vcvtq_f32_s32(vaddq_s32(
+                    vdotq_s32(zerov, v0_1l, v1_1l),
+                    vdotq_s32(zerov, v0_1h, v1_1h),
+                )),
+                f16::to_f32(ab1.d) * f16::to_f32(bb1.d),
+            );
+        }
+        vaddvq_f32(sumv0) + vaddvq_f32(sumv1) + summs
+    };
+
+    // handle the remaining blocks, it seems that only tinyllamas has the case where n_blocks % 2 != 0
+    for i in n_blocks_rounded..n_blocks {
+        let mut sumi = 0;
+        for j in 0..16 {
+            let v0 = (abs[i].qs[j] & 0x0F) as i32;
+            let v1 = ((abs[i].qs[j] >> 4) & 0x0F) as i32;
+
+            sumi += v0 * bbs[i].qs[j] as i32 + v1 * bbs[i].qs[j + 16] as i32;
+        }
+        sumf += (abs[i].d.to_f32() * bbs[i].d) * sumi as f32 + abs[i].m.to_f32() * bbs[i].s;
+    }
+    sumf
+}
+
+#[allow(unused)]
+pub fn vec_dot_q4_1_q8_1_fallback(abs: &[BlockQ4_1], bbs: &[BlockQ8_1]) -> f32 {
     let mut sumf = 0f32;
     for i in 0..abs.len() {
         let mut sumi: i32 = 0;

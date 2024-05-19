795c9a49b2d698245be9e9e04da78b87f32f45da
Li Yazhou <me.ssword@gmail.com>
2024-05-19 15:45:17 +0800

    add test cases for arithmetics

--- crabml-vulkan/src/vulkan_tensor.rs
+++ crabml-vulkan/src/vulkan_tensor.rs
@@ -268,4 +268,43 @@ mod tests {
         ]);
         Ok(())
     }
+
+    #[test]
+    fn test_scale_inplace() -> Result<()> {
+        let d = VulkanTensorDevice::new(VulkanTensorDeviceOptions::default());
+
+        let buf1 = (0..34).map(|v| v as f32).collect::<Vec<_>>();
+
+        let t1 = VulkanTensor::new(&buf1, &[34], d.clone()).unwrap();
+
+        let t1 = t1.scale_inplace(2.0).unwrap();
+        let mut bufo = vec![0.0; 34];
+        t1.export(&mut bufo)?;
+
+        assert_eq!(bufo, vec![
+            0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0,
+            30.0, 32.0, 34.0, 36.0, 38.0, 40.0, 42.0, 44.0, 46.0, 48.0, 50.0, 52.0, 54.0, 56.0,
+            58.0, 60.0, 62.0, 64.0, 66.0
+        ]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_div_scalar() -> Result<()> {
+        let d = VulkanTensorDevice::new(VulkanTensorDeviceOptions::default());
+
+        let buf1 = (0..32).map(|v| v as f32).collect::<Vec<_>>();
+
+        let t1 = VulkanTensor::new(&buf1, &[32], d.clone()).unwrap();
+
+        let t1 = t1.div_scalar_inplace(2.0).unwrap();
+        let mut bufo = vec![0.0; 32];
+        t1.export(&mut bufo)?;
+
+        assert_eq!(bufo, vec![
+            0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0,
+            8.5, 9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0, 12.5, 13.0, 13.5, 14.0, 14.5, 15.0, 15.5
+        ]);
+        Ok(())
+    }
 }

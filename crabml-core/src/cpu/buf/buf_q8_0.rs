a9db7974fc7e99686cbca1054003f54645f0ea3c
foxever <liuxf1986@gmail.com>
2024-12-28 17:40:42 +0800

    fix: intel avx2 (#208)
    * fix: intel avx2
    
    fix a typo in vec_dot_q8_0_q8_0_avx2
    
    * chore: cargo fmt

--- crabml-core/src/cpu/buf/buf_q8_0.rs
+++ crabml-core/src/cpu/buf/buf_q8_0.rs
@@ -228,7 +228,8 @@ fn vec_dot_q8_0_q8_0_neon(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
 pub fn vec_dot_q8_0_q8_0_avx2(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
     use std::arch::x86_64::*;
 
-    use crate::backends::cpu::archutil::x86_64::*;
+    use crate::cpu::archutil::x86_64::*;
+
     debug_assert_eq!(abs.len(), bbs.len());
 
     unsafe {
@@ -254,7 +255,7 @@ pub fn vec_dot_q8_0_q8_0_avx2(abs: &[BlockQ8_0], bbs: &[BlockQ8_0]) -> f32 {
 
         if abs.len() % 2 == 1 {
             let a = abs.last().unwrap_unchecked();
-            let b = abs.last().unwrap_unchecked();
+            let b = bbs.last().unwrap_unchecked();
 
             let d = _mm256_set1_ps(a.d.to_f32() * b.d.to_f32());
 
